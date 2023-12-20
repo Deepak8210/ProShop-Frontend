9b760fbf1977f05d77768d4904cf44ce5ac143e0
Deepak8210 <deepakrajput058@gmail.com>
2023-12-20 11:27:36 +0530

    changeinproxy

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://proshopbackend.onrender.com",
+      "/api": {
+        target: "https://proshopbackend.onrender.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
     },
+
+    plugins: [react()],
   },
-  plugins: [react()],
 });
